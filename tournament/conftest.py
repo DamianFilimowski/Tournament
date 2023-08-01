3f015b8351970172fa27341d0b0a78d5bb0ae653
DamianFilimowski <damianFilimowski@gmail.com>
2023-08-01 09:47:37 +0200

    tournament crud tests

--- tournament/conftest.py
+++ tournament/conftest.py
@@ -3,18 +3,30 @@ import pytest
 from tournament.models import *
 from accounts.models import *
 
+
 @pytest.fixture
 def user():
     u = CustomUser.objects.create(username='test_user')
     return u
 
+
 @pytest.fixture
-def user_not_captain():
+def user_not_creator():
     u = CustomUser.objects.create(username='test_user_two')
     return u
+
+
 @pytest.fixture
 def teams(user):
     lst = []
     for x in range(32):
         lst.append(Team.objects.create(name=x, short_name=x, captain=user))
+    return lst
+
+
+@pytest.fixture
+def tournaments(user):
+    lst = []
+    for x in range(5):
+        lst.append(Tournament.objects.create(name=x, max_teams_amount=x, tournament_admin=user))
     return lst
--- tournament/tests.py
+++ tournament/tests.py
@@ -1,7 +1,6 @@
 import pytest
 from django.core.exceptions import ObjectDoesNotExist
 from django.test import Client
-from django.urls import reverse
 
 from .models import *
 
@@ -66,13 +65,15 @@ def test_team_update_captain_get(teams, user):
     response = browser.get(url)
     assert response.status_code == 200
 
+
 @pytest.mark.django_db
-def test_team_update_not_captain_get(teams, user_not_captain):
+def test_team_update_not_captain_get(teams, user_not_creator):
     team = teams[0]
     url = reverse('tournament:team_update', kwargs={'pk': team.id})
-    browser.force_login(user_not_captain)
+    browser.force_login(user_not_creator)
     response = browser.get(url)
     assert response.status_code == 403
+
 
 @pytest.mark.django_db
 def test_team_update_captain_post(teams, user):
@@ -88,24 +89,40 @@ def test_team_update_captain_post(teams, user):
     assert Team.objects.get(**data)
     assert response.url.startswith(reverse('tournament:team_detail', kwargs={'pk': team.id}))
 
+
 @pytest.mark.django_db
-def test_team_delete_get_not_captain(teams, user_not_captain):
+def test_team_update_captain_post_too_long(teams, user):
+    team = teams[0]
+    url = reverse('tournament:team_update', kwargs={'pk': team.id})
+    browser.force_login(user)
+    data = {
+        'name': 'asdas',
+        'short_name': 'blassdsfsa'
+    }
+    response = browser.post(url, data)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_team_delete_not_captain_get(teams, user_not_creator):
     team = teams[0]
     url = reverse('tournament:team_delete', kwargs={'pk': team.id})
-    browser.force_login(user_not_captain)
+    browser.force_login(user_not_creator)
     response = browser.get(url)
     assert response.status_code == 403
 
+
 @pytest.mark.django_db
-def test_team_delete_get_captain(teams, user):
+def test_team_delete_captain_get(teams, user):
     team = teams[0]
     url = reverse('tournament:team_delete', kwargs={'pk': team.id})
     browser.force_login(user)
     response = browser.get(url)
     assert response.status_code == 200
 
+
 @pytest.mark.django_db
-def test_team_delete_post_captain(teams, user):
+def test_team_delete_captain_post(teams, user):
     team = teams[0]
     url = reverse('tournament:team_delete', kwargs={'pk': team.id})
     browser.force_login(user)
@@ -116,3 +133,103 @@ def test_team_delete_post_captain(teams, user):
     assert response.url.startswith(reverse('accounts:profile'))
 
 
+@pytest.mark.django_db
+def test_tournament_list(tournaments):
+    url = reverse('tournament:tournament_list')
+    response = browser.get(url)
+    assert response.status_code == 200
+    assert list(response.context['object_list']) == tournaments
+
+
+@pytest.mark.django_db
+def test_tournament_detail(tournaments):
+    tournament = tournaments[0]
+    url = reverse('tournament:tournament_detail', kwargs={'pk': tournament.id})
+    response = browser.get(url)
+    assert response.status_code == 200
+    assert response.context['object'] == tournament
+
+
+@pytest.mark.django_db
+def test_tournament_create_logged(user):
+    url = reverse('tournament:tournament_create')
+    data = {
+        'name': 'asd',
+        'max_teams_amount': 5,
+    }
+    browser.force_login(user)
+    response = browser.post(url, data)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('tournament:tournament_list'))
+    assert Tournament.objects.get(**data)
+
+
+@pytest.mark.django_db
+def test_tournament_create_not_logged():
+    url = reverse('tournament:tournament_create')
+    response = browser.get(url)
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_tournament_update_creator_get(tournaments, user):
+    tournament = tournaments[0]
+    url = reverse('tournament:tournament_update', kwargs={'pk': tournament.id})
+    browser.force_login(user)
+    response = browser.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_tournament_update_not_creator_get(tournaments, user_not_creator):
+    tournament = tournaments[0]
+    url = reverse('tournament:tournament_update', kwargs={'pk': tournament.id})
+    browser.force_login(user_not_creator)
+    response = browser.get(url)
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_tournament_update_creator_post(tournaments, user):
+    tournament = tournaments[0]
+    url = reverse('tournament:tournament_update', kwargs={'pk': tournament.id})
+    browser.force_login(user)
+    data = {
+        'name': 'asdas',
+        'max_teams_amount': 4
+    }
+    response = browser.post(url, data)
+    assert response.status_code == 302
+    assert Tournament.objects.get(**data)
+    assert response.url.startswith(reverse('tournament:tournament_detail', kwargs={'pk': tournament.id}))
+
+
+@pytest.mark.django_db
+def test_tournament_delete_not_creator_get(tournaments, user_not_creator):
+    tournament = tournaments[0]
+    url = reverse('tournament:tournament_delete', kwargs={'pk': tournament.id})
+    browser.force_login(user_not_creator)
+    response = browser.get(url)
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_tournament_delete_creator_get(tournaments, user):
+    tournament = tournaments[0]
+    url = reverse('tournament:tournament_delete', kwargs={'pk': tournament.id})
+    browser.force_login(user)
+    response = browser.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_tournament_delete_creator_post(tournaments, user):
+    tournament = tournaments[0]
+    url = reverse('tournament:tournament_delete', kwargs={'pk': tournament.id})
+    browser.force_login(user)
+    response = browser.post(url)
+    assert response.status_code == 302
+    with pytest.raises(ObjectDoesNotExist):
+        Tournament.objects.get(name=tournament.name, max_teams_amount=tournament.max_teams_amount,
+                               tournament_admin=tournament.tournament_admin)
+    assert response.url.startswith(reverse('accounts:profile'))
